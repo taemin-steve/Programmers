7c05dfbf7262b3172f5692c222fbeed64dd10ac2
EHmin <okchrisok@naver.com>
2023-08-13 09:00:31 +0900

    Priority Queue

---
+++ EHmin/PCCP/priorityQueue/prac.py
@@ -0,0 +1,22 @@
+from queue import PriorityQueue
+
+q = PriorityQueue()
+
+
+
+class Data:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        
+    def __lt__(self, other):
+        return self.b > other.b
+    
+a = Data(1,3)
+b = Data(3,1)
+
+
+q.put(a)
+q.put(b)
+
+print(q.get().a)
